8e306d3bd99286088089cf65e3a0cc8a880d4c22
Luke Dennis <134551861+lucas-The-Coder@users.noreply.github.com>
2025-08-01 15:30:28 +0000

    createProduct error

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,41 +1,48 @@
 import product from "../models/product.model.js";
 
 const getProducts = async (req, res) => {
-    try {
-        const products = await product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const products = await product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getSingleProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const { id } = req.params;
+    const productData = await product.findById(id);
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const createProduct = async (req, res) => {
-    try {
-        const product = await product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const productData = await product.create(req.body);
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const updateProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedProduct = await product.findByIdAndUpdate(id, req.body, { new: true });
-        if (!updatedProduct) {
-            return res.status(404).json({ message: `Cannot find product with id ${id}` });
-        }
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }}
+  try {
+    const { id } = req.params;
+    const updatedProduct = await product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find product with id ${id}` });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { getProducts, getSingleProduct, createProduct, updateProduct };
