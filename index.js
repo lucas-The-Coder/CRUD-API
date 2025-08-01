ad3d9b43fc11f335e9b015f66dc44844fe21c208
Luke Dennis <134551861+lucas-The-Coder@users.noreply.github.com>
2025-08-01 15:37:20 +0200

    set fixed url

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const Product = require("./models/product.model.js")
-const productRoute = require("./routes/product.route.js")
-const app = express()
+const API_URL = "http://localhost:3000";
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use("/api/products", productRoute)
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
-    res.send("Hello from Node API!");
+  res.send("Hello from Node API!");
 });
 
-mongoose.connect("mongodb+srv://admin:Luke10506@backenddb.zqqwr.mongodb.net/CRUD-API-project?retryWrites=true&w=majority&appName=BackendDB").then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://admin:Luke10506@backenddb.zqqwr.mongodb.net/CRUD-API-project?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
     console.log("Connected to MongoDB!");
     app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
-})
-.catch(() => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
     console.log("Connection to MongoDB failed!");
-});
+  });
